4a61edc94b8efc12a36ee93937f01eb474fea121
Simran Bhattarai <sim.bhattarai@gmail.com>
2020-04-11 14:11:34 -0600

    Initial Commit

---
+++ Locator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Locator
+//
+//  Created by Simran Bhattarai on 2020-04-11.
+//  Copyright © 2020 Simran Bhattarai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
